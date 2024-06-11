37ee7f244b59c63d7cba3f80d6490e33a7b37cc5
SergioQui2003 <sergioquirogab2003@gmail.com>
2024-06-11 11:11:15 -0500

    Cambios en la bd, y conn

--- includes/conn.php
+++ includes/conn.php
@@ -2,7 +2,7 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=tiendaonline";
+	private $server = "mysql:host=localhost;dbname=Magicandy";
 	private $username = "root";
 	private $password = "";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
--- tiendaonline.sql
+++ tiendaonline.sql
@@ -1,23 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `tiendaonline`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cart`
---
 
 CREATE TABLE `cart` (
   `id` int(11) NOT NULL,
@@ -26,19 +9,10 @@ CREATE TABLE `cart` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `cart`
---
-
 INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`) VALUES
 (2, 13, 14, 1),
-(3, 13, 13, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `category`
---
+(3, 13, 13, 1),
+(11, 14, 26, 45);
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
@@ -46,23 +20,13 @@ CREATE TABLE `category` (
   `cat_slug` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `category`
---
-
 INSERT INTO `category` (`id`, `name`, `cat_slug`) VALUES
-(2, 'CPUS', 'desktop-pc'),
-(3, 'CELULARES', 'tablets'),
-(16, 'TECLADOS', 'TECLADOS'),
-(17, 'DISCO DURO', 'DISCO DURO'),
-(19, 'MONITOR', 'MONITOR'),
-(20, 'LAPTOPS', 'LAPTOPS');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `details`
---
+(2, 'chocolates', 'chocolates'),
+(3, 'gomas', 'gomas'),
+(16, 'acidos', 'acidos'),
+(17, 'importados', 'importados'),
+(19, 'caseros', 'caseros'),
+(20, 'refrescos', 'refrescos');
 
 CREATE TABLE `details` (
   `id` int(11) NOT NULL,
@@ -70,12 +34,6 @@ CREATE TABLE `details` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -86,146 +44,81 @@ CREATE TABLE `products` (
   `price` double NOT NULL,
   `photo` varchar(200) NOT NULL,
   `date_view` date NOT NULL,
-  `counter` int(11) NOT NULL
+  `counter` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `products`
---
-
-INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `slug`, `price`, `photo`, `date_view`, `counter`) VALUES
-(13, 20, 'LAPTOP ASUS VIVOBOOK 15 OLED K513EA-L12004T INTEL CORE I5 8GB RAM 512GB SSD 15.6\'\'', '<h2>La laptop K513EA-L12004T de Asus cuenta con un potente procesador Intel Core i5, 8GB de RAM, almacenamiento SSD de 512GB y un elegante y resistente chasis de aluminio</h2>\r\n', 'laptop-asus-vivobook-15-oled-k513ea-l12004t-intel-core-i5-8gb-ram-512gb-ssd-15-6', 2700, 'laptop-asus-vivobook-15-oled-k513ea-l12004t-intel-core-i5-8gb-ram-512gb-ssd-15-6.jpg', '2022-02-22', 8),
-(14, 20, 'LAPTOP ACER AN515-55-50U5 INTEL CORE I5 10300H 8GB 512GB 15.6\"', '<h2>Nueva laptop gamer marca Acer con procesador Intel Core i5, memoria RAM de 8GB, Disco duro 512GB, FHD, 144Hz nitro y LED IPS. Encuentra las mejores ofertas en laptops solo en Ripley.</h2>\r\n', 'laptop-acer-an515-55-50u5-intel-core-i5-10300h-8gb-512gb-15-6', 3700, 'laptop-acer-an515-55-50u5-intel-core-i5-10300h-8gb-512gb-15-6_1645289044.jpg', '2022-02-22', 5),
-(15, 20, 'LAPTOP LENOVO LEGION 5 INTEL CORE I7 8GB 1TB + 256GB GEFORCE GTX 1650 15.6\"', '<h2>La laptop Legion 5 de Lenovo combina toda la potencia de los procesadores Intel y las gr&aacute;ficas de NVIDIA con una bater&iacute;a de larga duraci&oacute;n y un dise&ntilde;o elegante</h2>\r\n', 'laptop-lenovo-legion-5-intel-core-i7-8gb-1tb-256gb-geforce-gtx-1650-15-6', 4000, 'laptop-lenovo-legion-5-intel-core-i7-8gb-1tb-256gb-geforce-gtx-1650-15-6.jpg', '2022-02-22', 1),
-(16, 20, 'MACBOOK AIR 13.3\" APPLE M1 256GB 8GB', '<h2>Disfruta de toda la potencia y elegancia de la nueva Macbook Air, equipada con el poderoso Chip M1, el primero de una estirpe de procesadores dise&ntilde;ados espec&iacute;ficamente para tus productos Appl</h2>\r\n', 'macbook-air-13-3-apple-m1-256gb-8gb', 4500, 'macbook-air-13-3-apple-m1-256gb-8gb.jpg', '2022-02-19', 1),
-(17, 20, 'LAPTOP LENOVO IDEAPAD 5I INTEL CORE I7-1165G7 16GB 256GB SSD 15.6\"', '<h2>Nueva Lenovo IdeaPad 5i, con procesador Intel core de 11va generaci&oacute;n, Windows 10 Home - Espa&ntilde;ol, tarjeta gr&aacute;fica Intel Iris Xe Graphics (Integrada) y pantalla IPS Antireflejo. Encuentra en Ripley las mejores ofertas en laptops.</h2>\r\n', 'laptop-lenovo-ideapad-5i-intel-core-i7-1165g7-16gb-256gb-ssd-15-6', 3400, 'laptop-lenovo-ideapad-5i-intel-core-i7-1165g7-16gb-256gb-ssd-15-6_1645289380.jpg', '0000-00-00', 0),
-(18, 20, 'LAPTOP ASUS X415 X415EA-EB112T 8GB DDR4 256GB SSD 14\'\'', '<h2>Laptop X415EA-EB112T de Asus. Procesador Intel&reg; Core i3-1115G4. Memoria RAM de 8GB. 256GB de almacenamiento en estado s&oacute;lido. &iexcl;Adqui&eacute;rela ya en Ripley.com!</h2>\r\n', 'laptop-asus-x415-x415ea-eb112t-8gb-ddr4-256gb-ssd-14', 1700, 'laptop-asus-x415-x415ea-eb112t-8gb-ddr4-256gb-ssd-14.jpg', '2022-02-19', 2),
-(19, 20, 'LAPTOP ASUS FX516PC-HN004T INTEL CORE I7 16GB RAM 512GB SSD 15.6\'\'', '<h2>Si buscas una laptop compacta, potente y con un exclusivo dise&ntilde;o, la laptop FX516PC-HN004T es perfecta para ti</h2>\r\n', 'laptop-asus-fx516pc-hn004t-intel-core-i7-16gb-ram-512gb-ssd-15-6', 5000, 'laptop-asus-fx516pc-hn004t-intel-core-i7-16gb-ram-512gb-ssd-15-6.jpg', '2022-02-22', 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sales`
---
+INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `slug`, `price`, `photo`, `date_view`, `counter`, `quantity`) VALUES
+(13, 20, 'BonBum', '<h2>Una tentadora delicia que combina lo mejor de dos mundos: el crujido de una galleta y la suavidad de un relleno cremoso. Los bonbums suelen tener forma de peque&ntilde;as esferas o discos, con una capa exterior crujiente y un centro de crema que puede ser de chocolate, avellana o vainilla. Son perfectos para satisfacer antojos dulces y a&ntilde;adir un toque de indulgencia a cualquier momento del d&iacute;a.</h2>\r\n', 'bonbum', 3100, 'bonbum_1715224124.webp', '2024-05-30', 4, 44),
+(14, 3, 'Anisada', '<h2>Una deliciosa golosina con sabor a an&iacute;s, que se presenta en peque&ntilde;as pastillas redondas o en forma de gota. Su sabor suave y refrescante evoca notas de regaliz y hierbas, ofreciendo un placer dulce y arom&aacute;tico con cada mordida.</h2>\r\n', 'anisadas', 3700, 'anisadas_1715223950.webp', '2024-05-10', 1, 90),
+(15, 2, 'Gelatina', '<p>Una opci&oacute;n refrescante y divertida entre los dulces, la gelatina es una golosina que combina sabor y textura de manera &uacute;nica. Con una variedad de sabores que van desde frutales hasta cremosos, la gelatina puede presentarse en forma de cubos, figuras o incluso en capas multicolores. Su textura suave y ligeramente el&aacute;stica la convierte en un postre ligero y refrescante, perfecto para disfrutar en cualquier ocasi&oacute;n..</p>\r\n', 'gelatina', 4000, 'gelatina_1715224168.webp', '2024-05-30', 5, 0),
+(17, 20, 'Max', '<h2>Un cl&aacute;sico entre los dulces de caramelo, el Max es conocido por su sabor intenso y su textura suave y masticable. Estos caramelos suelen presentarse en forma de peque&ntilde;as tabletas o barras envueltas individualmente, ofreciendo una explosi&oacute;n de dulzura que perdura en el paladar. Con sabores que van desde la tradicional crema de leche hasta el ex&oacute;tico caramelo salado, el Max es una opci&oacute;n popular para satisfacer antojos de dulces en cualquier momento del d&iacute;a.</h2>\r\n', 'max', 3400, 'max_1715224144.webp', '2024-05-29', 1, 12),
+(18, 20, 'Fruticas', '<h2>Peque&ntilde;as y coloridas, las fruticas son una explosi&oacute;n de sabor en cada mordisco. Estas golosinas gelatinosas vienen en una variedad de formas y sabores que representan frutas reales, como fresa, manzana, cereza, naranja y pi&ntilde;a. Su textura suave y jugosa las hace irresistibles para los amantes de los dulces, ofreciendo una experiencia de dulzura fresca y frutal..</h2>\r\n', 'fruticas', 1700, 'fruticas_1715224177.webp', '2024-05-09', 1, 44),
+(19, 3, 'Barriletete', '<h2>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</h2>\r\n', 'barrilete', 5000, 'barrilete_1715224115.webp', '2024-05-09', 2, 44),
+(21, 2, 'deditos', '<h2>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</h2>\r\n', 'rjrf', 9000, 'rjrf_1717025664.webp', '0000-00-00', 0, 44),
+(22, 3, 'Super Coco', '<h2>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</h2>\r\n', 'otro', 3700, 'otro_1717025572.webp', '0000-00-00', 0, 44),
+(23, 3, 'Moritas', '<p>gtgtgtg</p>\r\n', 'gtgtgtg', 4000, 'gtgtgtg_1717024460.webp', '0000-00-00', 0, 89),
+(24, 3, 'Candy Ranch', '<h2>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</h2>\r\n', 'otrooooo', 3700, 'otrooooo_1717025608.webp', '0000-00-00', 0, 12),
+(25, 2, 'pandas', '<p>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</p>\r\n', 'pandas', 6500, 'pandas_1717025643.webp', '0000-00-00', 0, 44),
+(26, 3, 'Sonrics', '<h2>Un dulce cl&aacute;sico que recuerda a la infancia, el barrilete es un caramelo suave y masticable, a menudo con forma de barril o cilindro. Viene en una variedad de sabores frutales, desde fresa y lim&oacute;n hasta naranja y uva, ofreciendo una explosi&oacute;n de dulzura en cada bocado</h2>\r\n', 'sonrrr', 4000, 'sonrrr_1717025698.webp', '0000-00-00', 0, 65);
 
 CREATE TABLE `sales` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `pay_id` varchar(50) NOT NULL,
-  `sales_date` date NOT NULL
+  `total` double NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
+INSERT INTO `sales` (`id`, `total`, `date`) VALUES
+(1, 4000, '2023-03-01 03:05:28'),
+(2, 4500, '2023-03-05 23:15:28'),
+(3, 5000, '2023-03-13 19:05:28');
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `type` int(1) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `address` text NOT NULL,
-  `contact_info` varchar(100) NOT NULL,
-  `photo` varchar(200) NOT NULL,
-  `status` int(1) NOT NULL,
-  `activate_code` varchar(15) NOT NULL,
-  `reset_code` varchar(15) NOT NULL,
-  `created_on` date NOT NULL
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `users`
---
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `type`) VALUES
+(13, 'Paco', 'Paco', 'paco@paco.com', 'Cliente'),
+(14, 'Luis', 'Luis', 'luis@luis.com', 'Cliente');
 
-INSERT INTO `users` (`id`, `email`, `password`, `type`, `firstname`, `lastname`, `address`, `contact_info`, `photo`, `status`, `activate_code`, `reset_code`, `created_on`) VALUES
-(1, 'admin@gmail.com', '$2y$10$VZUxZl4MAEC0vZSH2tH4fOgQ5mNIfergHCpq4FtHTB05RUazH5hiu', 1, 'Tarea', 'Completo', '', '', 'tarea.png', 1, '', '', '2018-05-01'),
-(13, 'joel@gmail.com', '$2y$10$M566C0GZvd8pq1UQ/4XzMOx59isKq.3qvVWkadyeJftvlwbtysIMW', 0, 'Joel', 'Tarea', 'Tarea', '876745653', 'tarea.png', 1, '', '', '2022-02-19');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cart`
---
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `category`
---
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `details`
---
 ALTER TABLE `details`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `sales`
---
 ALTER TABLE `sales`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cart`
---
 ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+ALTER TABLE `details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+ALTER TABLE `sales`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT de la tabla `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT de la tabla `details`
---
-ALTER TABLE `details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT de la tabla `sales`
---
-ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
